b83bd2fc481f459e0c40273af9e0b93f859e87a8
NGrijalba <nahug07@gmail.com>
2023-05-29 18:37:10 -0300

    Refactor: functions

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "fmt"
 import "bufio"
 import "strings"
 
+var reader *bufio.Reader
+
 func crearUsuario() {
 	fmt.Println("crear usuario")
 }
@@ -21,37 +23,48 @@ func eliminarUsuario() {
 	fmt.Println("eliminar usuario")
 }
 
+func readLine() string {
+
+	if option, err := reader.ReadString('\n'); err != nil {
+		panic("No es posible obtener el valor")
+	} else {
+		return strings.TrimSuffix(option, "\n")
+	}
+}
+
 func main() {
 
-	var reader *bufio.Reader
 	var option string
 
 	reader = bufio.NewReader(os.Stdin)
 
-	fmt.Println("A) Crear")
-	fmt.Println("B) Listar")
-	fmt.Println("C) Actualizar")
-	fmt.Println("D) Eliminar")
+	for {
+		fmt.Println("A) Crear")
+		fmt.Println("B) Listar")
+		fmt.Println("C) Actualizar")
+		fmt.Println("D) Eliminar")
 
-	fmt.Println("Ingresa una opción válida: ")
-	option, err := reader.ReadString('\n')
+		fmt.Println("Ingresa una opción válida: ")
 
-	if err != nil {
-		panic("No es posible obtener el valor")
+		option = readLine()
+
+		if option == "salir" || option == "s" {
+			break
+		}
+
+		switch option {
+			case "a", "crear":
+				crearUsuario()
+			case "b", "listar":
+				listarUsuarios()
+			case "c", "actualizar":
+				actualizarUsuario()
+			case "d", "eliminar":
+				eliminarUsuario()
+			default:
+				fmt.Println("Opción no válida")				
+		}
 	}
 
-	option = strings.TrimSuffix(option, "\n")
-
-	switch option {
-	case "a", "crear":
-		crearUsuario()
-	case "b", "listar":
-		listarUsuarios()
-	case "c", "actualizar":
-		actualizarUsuario()
-	case "d", "eliminar":
-		eliminarUsuario()
-	default:
-		fmt.Println("Opción no válida")				
-	}
+	fmt.Println("Adiós 0_0")
 }
